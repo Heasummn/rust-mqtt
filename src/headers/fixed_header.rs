295b76176dea20d10e729ae2fb46a63333f741d6
Hassam Uddin <hassamuddin20@gmail.com>
2020-03-24 13:46:45 -0500

    ConAck loop setup

--- src/headers/fixed_header.rs
+++
@@ -1,6 +0,0 @@
-use super::header_type::HeaderType;
-
-pub struct FixedHeader {
-    pub header_type: HeaderType,
-    
-}
--- src/headers/mod.rs
+++
@@ -1,4 +0,0 @@
-pub use header_type::HeaderType;
-
-pub mod fixed_header;
-pub mod header_type;
